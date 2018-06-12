c5237033ee231c83383b750a754f76d3c11418dd
Chris DePaul <chris.depaul@rangle.io>
2018-06-12 07:51:48 -0400

    all

---
+++ data/team-rounds.js
@@ -0,0 +1,163 @@
+const x = JSON.stringify({
+  RUS: {
+    name: 'Russia',
+    group: 'A',
+    round: 0,
+  },
+  KSA: {
+    name: 'Saudi Arabia',
+    group: 'A',
+    round: 0,
+  },
+  EGY: {
+    name: 'Egypt',
+    group: 'A',
+    round: 0,
+  },
+  URU: {
+    name: 'Uruguay',
+    group: 'A',
+    round: 0,
+  },
+  POR: {
+    name: 'Portugal',
+    group: 'B',
+    round: 0,
+  },
+  ESP: {
+    name: 'Spain',
+    group: 'B',
+    round: 0,
+  },
+  MAR: {
+    name: 'Morocco',
+    group: 'B',
+    round: 0,
+  },
+  IRN: {
+    name: 'Iran',
+    group: 'B',
+    round: 0,
+  },
+  FRA: {
+    name: 'France',
+    group: 'C',
+    round: 0,
+  },
+  AUS: {
+    name: 'Australia',
+    group: 'C',
+    round: 0,
+  },
+  PER: {
+    name: 'Peru',
+    group: 'C',
+    round: 0,
+  },
+  DEN: {
+    name: 'Denmark',
+    group: 'C',
+    round: 0,
+  },
+  ARG: {
+    name: 'Argentina',
+    group: 'D',
+    round: 0,
+  },
+  ISL: {
+    name: 'Iceland',
+    group: 'D',
+    round: 0,
+  },
+  CRO: {
+    name: 'Croatia',
+    group: 'D',
+    round: 0,
+  },
+  NGA: {
+    name: 'Nigeria',
+    group: 'D',
+    round: 0,
+  },
+  BRA: {
+    name: 'Brazil',
+    group: 'E',
+    round: 0,
+  },
+  SUI: {
+    name: 'Switzerland',
+    group: 'E',
+    round: 0,
+  },
+  CRC: {
+    name: 'Costa Rica',
+    group: 'E',
+    round: 0,
+  },
+  SRB: {
+    name: 'Serbia',
+    group: 'E',
+    round: 0,
+  },
+  GER: {
+    name: 'Germany',
+    group: 'F',
+    round: 0,
+  },
+  MEX: {
+    name: 'Mexico',
+    group: 'F',
+    round: 0,
+  },
+  SWE: {
+    name: 'Sweden',
+    group: 'F',
+    round: 0,
+  },
+  KOR: {
+    name: 'Korea Republic',
+    group: 'F',
+    round: 0,
+  },
+  BEL: {
+    name: 'Belgium',
+    group: 'G',
+    round: 0,
+  },
+  PAN: {
+    name: 'Panama',
+    group: 'G',
+    round: 0,
+  },
+  TUN: {
+    name: 'Tunisia',
+    group: 'G',
+    round: 0,
+  },
+  ENG: {
+    name: 'England',
+    group: 'G',
+    round: 0,
+  },
+  POL: {
+    name: 'Poland',
+    group: 'H',
+    round: 0,
+  },
+  SEN: {
+    name: 'Senegal',
+    group: 'H',
+    round: 0,
+  },
+  COL: {
+    name: 'Columbia',
+    group: 'H',
+    round: 0,
+  },
+  JPN: {
+    name: 'Japan',
+    group: 'H',
+    round: 0,
+  },
+});
+console.log(x);
