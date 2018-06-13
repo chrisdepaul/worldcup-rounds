7be4de6a8c4b7e13a957821e1bdfb3481916d656
Chris DePaul <chris.depaul@rangle.io>
2018-06-13 13:39:03 -0400

    real test

--- src/index.js
+++ src/index.js
@@ -10,118 +10,124 @@ import scoring from './config/scoring';
 import http from 'http';
 import createHandler from 'github-webhook-handler';
 
-const participants = [
-  {
-    name: 'Chris',
-    email: 'cdepaul@gmail.com',
-    teams: ['URU', 'ESP', 'FRA', 'NGA', 'BRA', 'GER', 'BEL', 'COL'],
-  },
-];
-const handler = createHandler({
-  path: '/webhook',
-  secret: process.env.WEBHOOK_SECRET,
-});
-
 http
   .createServer(function(req, res) {
-    handler(req, res, function(err) {
-      res.statusCode = 404;
-      res.end('no such location');
-    });
+    console.log('listning...')
   })
   .listen(8080);
 
-handler.on('error', function(err) {
-  console.error('Error:', err.message);
-});
+// const participants = [
+//   {
+//     name: 'Chris',
+//     email: 'cdepaul@gmail.com',
+//     teams: ['URU', 'ESP', 'FRA', 'NGA', 'BRA', 'GER', 'BEL', 'COL'],
+//   },
+// ];
+// const handler = createHandler({
+//   path: '/webhook',
+//   secret: process.env.WEBHOOK_SECRET,
+// });
 
-handler.on('push', function(event) {
-  console.log(
-    'Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref,
-  );
+// http
+//   .createServer(function(req, res) {
+//     handler(req, res, function(err) {
+//       res.statusCode = 404;
+//       res.end('no such location');
+//     });
+//   })
+//   .listen(8080);
 
-  updateLeaderBoard();
-});
+// handler.on('error', function(err) {
+//   console.error('Error:', err.message);
+// });
 
-const send = gmail({
-  user: process.env.EMAIL,
-  pass: process.env.PASSWORD,
-});
+// handler.on('push', function(event) {
+//   console.log(
+//     'Received a push event for %s to %s',
+//     event.payload.repository.name,
+//     event.payload.ref,
+//   );
 
-const getStandings = async () => {
-  return await request({
-    uri:
-      'https://raw.githubusercontent.com/chrisdepaul/worldcup-rounds/master/data/team-rounds.json',
-    json: true,
-  });
-};
+//   updateLeaderBoard();
+// });
 
-const sendEmails = async (poolStandings, rounds, emails) => {
-  const rows = poolStandings
-    .map(
-      standing =>
-        `<tr style="border: solid 1px"><td>${standing.name}</td><td>${
-          standing.points
-        }</td></tr>`,
-    )
-    .reduce((con, item) => {
-      return `${con}${item}`;
-    }, '');
+// const send = gmail({
+//   user: process.env.EMAIL,
+//   pass: process.env.PASSWORD,
+// });
 
-  try {
-    await send({
-      to: emails,
-      subject: `Worldcup Standings: ${moment().format(
-        'MMMM Do YYYY, h:mm:ss a',
-      )}`,
-      html: `
-      <p>Hello Everyone! This is ChrisBot, a bot programmed by Chris to send you the ongoing results of the pool. I might be fraught with problems, but love me, like Real Chris, all the same. Notify Miles DePaul about any score disputes I report.</p>
-      <p>These points were recorded after round: ${rounds}.</p>
-      <table style="width: 50%; border: solid 1px; text-align: center;">
-        <tr style="border: solid 1px">
-          <td><strong>Person</strong></td>
-          <td><strong>Points</strong></td>
-        </tr>
-        ${rows}
-      </table>
-    `,
-    });
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
+// const getStandings = async () => {
+//   return await request({
+//     uri:
+//       'https://raw.githubusercontent.com/chrisdepaul/worldcup-rounds/master/data/team-rounds.json',
+//     json: true,
+//   });
+// };
 
-const updateLeaderBoard = async () => {
-  try {
-    const teamStandings = await getStandings();
+// const sendEmails = async (poolStandings, rounds, emails) => {
+//   const rows = poolStandings
+//     .map(
+//       standing =>
+//         `<tr style="border: solid 1px"><td>${standing.name}</td><td>${
+//           standing.points
+//         }</td></tr>`,
+//     )
+//     .reduce((con, item) => {
+//       return `${con}${item}`;
+//     }, '');
 
-    const poolStandings = pool
-      .map(participant => {
-        const points = participant.teams.reduce((totalPoints, team) => {
-          const round = teamStandings[team].round;
-          const pointsFromTeam = round * scoring[round].pts;
-          return totalPoints + pointsFromTeam;
-        }, 0);
-        return {
-          ...participant,
-          points,
-        };
-      })
-      .sort((a, b) => a - b);
+//   try {
+//     await send({
+//       to: emails,
+//       subject: `Worldcup Standings: ${moment().format(
+//         'MMMM Do YYYY, h:mm:ss a',
+//       )}`,
+//       html: `
+//       <p>Hello Everyone! This is ChrisBot, a bot programmed by Chris to send you the ongoing results of the pool. I might be fraught with problems, but love me, like Real Chris, all the same. Notify Miles DePaul about any score disputes I report.</p>
+//       <p>These points were recorded after round: ${rounds}.</p>
+//       <table style="width: 50%; border: solid 1px; text-align: center;">
+//         <tr style="border: solid 1px">
+//           <td><strong>Person</strong></td>
+//           <td><strong>Points</strong></td>
+//         </tr>
+//         ${rows}
+//       </table>
+//     `,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// };
 
-    const completedRounds = Object.keys(teamStandings).map(
-      key => teamStandings[key].round,
-    );
-    const maxRound = Math.max(...completedRounds);
+// const updateLeaderBoard = async () => {
+//   try {
+//     const teamStandings = await getStandings();
 
-    const participantEmails = participants.map(
-      participant => participant.email,
-    );
-    return sendEmails(poolStandings, maxRound, participantEmails);
-  } catch (error) {
-    console.log(error);
-  }
+//     const poolStandings = pool
+//       .map(participant => {
+//         const points = participant.teams.reduce((totalPoints, team) => {
+//           const round = teamStandings[team].round;
+//           const pointsFromTeam = round * scoring[round].pts;
+//           return totalPoints + pointsFromTeam;
+//         }, 0);
+//         return {
+//           ...participant,
+//           points,
+//         };
+//       })
+//       .sort((a, b) => a - b);
+
+//     const completedRounds = Object.keys(teamStandings).map(
+//       key => teamStandings[key].round,
+//     );
+//     const maxRound = Math.max(...completedRounds);
+
+//     const participantEmails = participants.map(
+//       participant => participant.email,
+//     );
+//     return sendEmails(poolStandings, maxRound, participantEmails);
+//   } catch (error) {
+//     console.log(error);
+//   }
 };
