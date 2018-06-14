ec5433b7fa71d419290d4f618a1f08aa962c3efd
Chris DePaul <chris.depaul@rangle.io>
2018-06-13 23:15:19 -0400

    code update

--- src/index.js
+++ src/index.js
@@ -3,22 +3,14 @@ import request from 'request-promise';
 import { flatten, pathOr, sort } from 'ramda';
 import gmail from 'gmail-send';
 import moment from 'moment';
-// import participants from './config/participants';
+import participants from './config/participants';
 import scoring from './config/scoring';
 
 import http from 'http';
 import createHandler from 'github-webhook-handler';
 
-const port = process.env.PORT;
-const API_FILE = process.env.API_FILE;
+const { PORT, API_BASE, API_FILE } = process.env;
 
-// const participants = [
-//   {
-//     name: 'Chris',
-//     email: 'cdepaul@gmail.com',
-//     teams: ['URU', 'ESP', 'FRA', 'NGA', 'BRA', 'GER', 'BEL', 'COL'],
-//   },
-// ];
 const handler = createHandler({
   path: '/webhook',
   secret: process.env.WEBHOOK_SECRET,
@@ -31,9 +23,9 @@ http
       res.end('no such location');
     });
   })
-  .listen(port);
+  .listen(PORT);
 
-console.log(`listening on port ${port}...`);
+console.log(`listening on port ${PORT}...`);
 
 handler.on('error', function(err) {
   console.error('Error:', err.message);
@@ -50,88 +42,87 @@ handler.on('push', function(event) {
   console.log(`Received a push event! Changed files: ${modifiedFiles}.`);
 
   if (modifiedFiles.includes(API_FILE)) {
-    console.log(`The API File Changed!`);
+    console.log(`${API_FILE} updated!`);
+    updateLeaderBoard();
   }
-  // updateLeaderBoard();
 });
 
-// const send = gmail({
-//   user: process.env.EMAIL,
-//   pass: process.env.PASSWORD,
-// });
+const send = gmail({
+  user: process.env.EMAIL,
+  pass: process.env.PASSWORD,
+});
 
-// const getStandings = async () => {
-//   return await request({
-//     uri:
-//       'https://raw.githubusercontent.com/chrisdepaul/worldcup-rounds/master/data/team-rounds.json',
-//     json: true,
-//   });
-// };
+const getStandings = async () => {
+  return await request({
+    uri: `${API_BASE}${API_FILE}`,
+    json: true,
+  });
+};
 
-// const sendEmails = async (poolStandings, rounds, emails) => {
-//   const rows = poolStandings
-//     .map(
-//       standing =>
-//         `<tr style="border: solid 1px"><td>${standing.name}</td><td>${
-//           standing.points
-//         }</td></tr>`,
-//     )
-//     .reduce((con, item) => {
-//       return `${con}${item}`;
-//     }, '');
+const sendEmails = async (poolStandings, rounds, emails) => {
+  const rows = poolStandings
+    .map(
+      standing =>
+        `<tr style="border: solid 1px"><td>${standing.name}</td><td>${
+          standing.points
+        }</td></tr>`,
+    )
+    .reduce((con, item) => {
+      return `${con}${item}`;
+    }, '');
 
-//   try {
-//     await send({
-//       to: emails,
-//       subject: `Worldcup Standings: ${moment().format(
-//         'MMMM Do YYYY, h:mm:ss a',
-//       )}`,
-//       html: `
-//       <p>Hello Everyone! This is ChrisBot, a bot programmed by Chris to send you the ongoing results of the pool. I might be fraught with problems, but love me, like Real Chris, all the same. Notify Miles DePaul about any score disputes I report.</p>
-//       <p>These points were recorded after round: ${rounds}.</p>
-//       <table style="width: 50%; border: solid 1px; text-align: center;">
-//         <tr style="border: solid 1px">
-//           <td><strong>Person</strong></td>
-//           <td><strong>Points</strong></td>
-//         </tr>
-//         ${rows}
-//       </table>
-//     `,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     throw error;
-//   }
-// };
+  try {
+    await send({
+      to: emails,
+      subject: `Worldcup Standings: ${moment().format(
+        'MMMM Do YYYY, h:mm:ss a',
+      )}`,
+      html: `
+      <p>Hello Everyone! This is ChrisBot, a bot programmed by Chris to send you the ongoing results of the pool. I might be fraught with problems, but love me, like Real Chris, all the same. Notify Miles DePaul about any score disputes I report.</p>
+      <p>These points were recorded after round: ${rounds}.</p>
+      <table style="width: 50%; border: solid 1px; text-align: center;">
+        <tr style="border: solid 1px">
+          <td><strong>Person</strong></td>
+          <td><strong>Points</strong></td>
+        </tr>
+        ${rows}
+      </table>
+    `,
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
-// const updateLeaderBoard = async () => {
-//   try {
-//     const teamStandings = await getStandings();
+const updateLeaderBoard = async () => {
+  try {
+    const teamStandings = await getStandings();
 
-//     const poolStandings = participants
-//       .map(participant => {
-//         const points = participant.teams.reduce((totalPoints, team) => {
-//           const round = teamStandings[team].round;
-//           const pointsFromTeam = round * scoring[round].pts;
-//           return totalPoints + pointsFromTeam;
-//         }, 0);
-//         return {
-//           ...participant,
-//           points,
-//         };
-//       })
-//       .sort((a, b) => a - b);
+    const poolStandings = participants
+      .map(participant => {
+        const points = participant.teams.reduce((totalPoints, team) => {
+          const round = teamStandings[team].round;
+          const pointsFromTeam = round * scoring[round].pts;
+          return totalPoints + pointsFromTeam;
+        }, 0);
+        return {
+          ...participant,
+          points,
+        };
+      })
+      .sort((a, b) => a - b);
 
-//     const completedRounds = Object.keys(teamStandings).map(
-//       key => teamStandings[key].round,
-//     );
-//     const maxRound = Math.max(...completedRounds);
+    const completedRounds = Object.keys(teamStandings).map(
+      key => teamStandings[key].round,
+    );
+    const maxRound = Math.max(...completedRounds);
 
-//     const participantEmails = participants.map(
-//       participant => participant.email,
-//     );
-//     return sendEmails(poolStandings, maxRound, participantEmails);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+    const participantEmails = participants.map(
+      participant => participant.email,
+    );
+    return sendEmails(poolStandings, maxRound, participantEmails);
+  } catch (error) {
+    console.log(error);
+  }
+};
